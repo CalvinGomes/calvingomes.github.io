b9d16a77803da60a2743c15a0141de85cb36a1aa
Calvin Gomes <calvingomes045@gmail.com>
2019-03-05 23:03:21 +0530

    Added about content and contact form

--- script/script.js
+++ script/script.js
@@ -12,4 +12,12 @@ $('document').ready(function () {
             
         });
     });
+
+    $( '.checkInput' ).keyup(function() {
+        if( $(this).val() ) {
+           $(this).addClass('not-empty');
+        } else {
+           $(this).removeClass('not-empty');
+        }
+      });
 });
