e63a2fb117fe54873da8419c65064e3ef8e2b158
Calvin Gomes <calvingomes045@gmail.com>
2019-03-08 21:56:00 +0530

    Added animations

--- script/script.js
+++ script/script.js
@@ -1,23 +1,36 @@
 $('document').ready(function () {
 
-    $(function() {
-        $(window).scroll(function() {    
+    $(function () {
+        $(window).scroll(function () {
             var scroll = $(window).scrollTop();
-        
+
             if (scroll >= 1) {
                 $(".navbar").addClass("notHome").removeClass("home");
             } else {
                 $(".navbar").removeClass("notHome").addClass("home");
             }
-            
+
         });
     });
 
-    $( '.checkInput' ).keyup(function() {
-        if( $(this).val() ) {
-           $(this).addClass('not-empty');
+    $('.checkInput').keyup(function () {
+        if ($(this).val()) {
+            $(this).addClass('not-empty');
         } else {
-           $(this).removeClass('not-empty');
+            $(this).removeClass('not-empty');
         }
-      });
+    });
+
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
